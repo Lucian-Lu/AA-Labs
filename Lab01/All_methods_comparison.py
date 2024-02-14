09345289f47b47bef848f0bef4447dbd2a01835f
Lupan <lucian.lupan@isa.utm.md>
2024-02-14 12:30:29 +0200

    Update first lab with fast doubling method and all_methods_comparison.py

--- Lab01/All_methods_comparison.py
+++ Lab01/All_methods_comparison.py
@@ -1,15 +1,12 @@
 import timeit
 import matplotlib.pyplot as plt
 import numpy as np
-import sys
 from decimal import Decimal, getcontext
 from math import sqrt
 
-sys.setrecursionlimit(3000)
 getcontext().prec = 1000
 MOD = 1000000007
 res = [0] * 2
-
 
 def recursive_fibonacci(nr):
     if nr == 0:
@@ -19,27 +16,17 @@ def recursive_fibonacci(nr):
     else:
         return recursive_fibonacci(nr - 1) + recursive_fibonacci(nr - 2)
 
+def calculate_execution_times_recursive(n, color):
+    execution_times = np.zeros(n + 1, dtype=float)
 
-def calculate_execution_times(n):
-    execution_times = np.zeros(n+1, dtype=float)
-    terms_to_print = np.unique(np.linspace(5, n, 17, dtype=int))
-
-    for term in range(n+1):
+    for term in range(n + 1):
         start_time = timeit.default_timer()
-        result = recursive_fibonacci(term)
+        recursive_fibonacci(term)
         end_time = timeit.default_timer()
         execution_time = (end_time - start_time) * 1000
         execution_times[term] = execution_time
 
-        if term in terms_to_print:
-            print(f"Fibonacci({term}), Execution Time: {execution_time} milliseconds")
-
-    plt.plot(range(n+1), execution_times, marker='o')
-    plt.title('Execution Time for Recursive Method')
-    plt.xlabel('Term')
-    plt.ylabel('Execution Time (milliseconds)')
-    plt.show()
-
+    plt.plot(range(n + 1), execution_times, marker='o', label=f'Recursive ({color})')
 
 def dynamic_fibonacci(nr, cache=None):
     if cache is None:
@@ -55,7 +42,7 @@ def dynamic_fibonacci(nr, cache=None):
             cache[nr - 2] = dynamic_fibonacci(nr - 2, cache)
         return cache[nr - 1] + cache[nr - 2]
 
-def calculate_execution_times_dynamic(n):
+def calculate_execution_times_dynamic(n, color):
     execution_times = []
     terms_to_print = np.unique(np.linspace(5, n, 17, dtype=int))
 
@@ -66,17 +53,10 @@ def calculate_execution_times_dynamic(n):
         execution_time = (end_time - start_time) * 1000
         execution_times.append(execution_time)
 
-        print(f"Fibonacci({term}), Execution Time: {execution_time} milliseconds")
-
-    plt.plot(terms_to_print, execution_times, marker='o')
-    plt.title('Dynamic Fibonacci Execution Time')
-    plt.xlabel('Term')
-    plt.ylabel('Execution Time (milliseconds)')
-    plt.show()
-
+    plt.plot(terms_to_print, execution_times, marker='o', label=f'Dynamic ({color})')
 
 def iterative_fibonacci(nr, cache=None):
-    if cache == None:
+    if cache is None:
         cache = [0, 1]
     if nr == 0:
         return cache[0]
@@ -87,7 +67,7 @@ def iterative_fibonacci(nr, cache=None):
             cache.append(cache[i] + cache[i - 1])
         return cache[nr]
 
-def calculate_execution_times_iterative(n):
+def calculate_execution_times_iterative(n, color):
     execution_times = []
     terms_to_print = np.unique(np.linspace(5, n, 17, dtype=int))
 
@@ -98,14 +78,7 @@ def calculate_execution_times_iterative(n):
         execution_time = (end_time - start_time) * 1000
         execution_times.append(execution_time)
 
-        print(f"Fibonacci({term}), Execution Time: {execution_time} milliseconds")
-
-    plt.plot(terms_to_print, execution_times, marker='o')
-    plt.title('Iterative Fibonacci Execution Time')
-    plt.xlabel('Term')
-    plt.ylabel('Execution Time (milliseconds)')
-    plt.show()
-
+    plt.plot(terms_to_print, execution_times, marker='o', label=f'Iterative ({color})')
 
 def fibonacci_binet(nr):
     sqrt_5 = Decimal(sqrt(5))
@@ -114,48 +87,18 @@ def fibonacci_binet(nr):
     fib_n = (golden_ratio**nr - (-1 / golden_ratio)**nr) / sqrt_5
     return round(fib_n)
 
-def calculate_execution_times_binet(n):
+def calculate_execution_times_binet(n, color):
     execution_times = []
-    errors = []
     terms_to_print = np.unique(np.linspace(5, n, 17, dtype=int))
 
     for term in terms_to_print:
         start_time = timeit.default_timer()
-        result = fibonacci_binet(term)
+        fibonacci_binet(term)
         end_time = timeit.default_timer()
         execution_time = (end_time - start_time) * 1000
         execution_times.append(execution_time)
-        true_result = iterative_fibonacci(term)
-        error = abs(result - true_result)
-        errors.append(error)
-        # , Error: {error}
-        print(f"Fibonacci({term}), Execution Time: {execution_time} milliseconds")
-    # print(errors)
-    plt.plot(terms_to_print, execution_times, marker='o')
-    plt.title('Binet Formula Fibonacci Execution Time')
-    plt.xlabel('Term')
-    plt.ylabel('Execution Time (milliseconds)')
-    plt.show()
 
-    plt.plot(terms_to_print, errors, marker='o')
-    plt.title('Binet Formula Errors')
-    plt.xlabel('Term')
-    plt.ylabel('Error')
-    plt.show()
-
-
-def multiply(a, b):
-    mul = [[0 for x in range(3)] for y in range(3)]
-    for i in range(3):
-        for j in range(3):
-            mul[i][j] = 0
-            for k in range(3):
-                mul[i][j] += a[i][k] * b[k][j]
-
-    for i in range(3):
-        for j in range(3):
-            a[i][j] = mul[i][j]
-    return a
+    plt.plot(terms_to_print, execution_times, marker='o', label=f'Binet Formula ({color})')
 
 def power(F, n):
     M = [[1, 1, 1], [1, 0, 0], [0, 1, 0]]
@@ -168,30 +111,37 @@ def power(F, n):
     return F[0][0] + F[0][1]
 
 
+def multiply(a, b):
+    mul = [[0 for x in range(3)]
+           for y in range(3)]
+    for i in range(3):
+        for j in range(3):
+            mul[i][j] = 0
+            for k in range(3):
+                mul[i][j] += a[i][k] * b[k][j]
+
+    for i in range(3):
+        for j in range(3):
+            a[i][j] = mul[i][j]
+    return a
+
+
 def findNthTerm(n):
     F = [[1, 1, 1], [1, 0, 0], [0, 1, 0]]
     return power(F, n - 2)
 
-
-def calculate_execution_times_matrix_power(n):
+def calculate_execution_times_matrix_power(n, color):
     execution_times = []
     terms_to_print = np.unique(np.linspace(5, n, 17, dtype=int))
 
     for term in terms_to_print:
         start_time = timeit.default_timer()
-        result = findNthTerm(term)
+        findNthTerm(term)
         end_time = timeit.default_timer()
         execution_time = (end_time - start_time) * 1000
         execution_times.append(execution_time)
 
-        print(f"Fibonacci({term}), Execution Time: {execution_time} milliseconds")
-
-    plt.plot(terms_to_print, execution_times, marker='o')
-    plt.title('Matrix Power Fibonacci Execution Time')
-    plt.xlabel('Term')
-    plt.ylabel('Execution Time (milliseconds)')
-    plt.show()
-
+    plt.plot(terms_to_print, execution_times, marker='o', label=f'Matrix Power ({color})')
 
 def FastDoubling(n, res):
     if n == 0:
@@ -214,7 +164,7 @@ def FastDoubling(n, res):
         res[0] = d
         res[1] = c + d
 
-def calculate_execution_times_fast_doubling(n):
+def calculate_execution_times_fast_doubling(n, color):
     execution_times = []
     terms_to_print = np.unique(np.linspace(5, n, 17, dtype=int))
 
@@ -225,36 +175,20 @@ def calculate_execution_times_fast_doubling(n):
         execution_time = (end_time - start_time) * 1000
         execution_times.append(execution_time)
 
-        print(f"Fibonacci({term}), Execution Time: {execution_time} milliseconds")
+    plt.plot(terms_to_print, execution_times, marker='o', label=f'Fast Doubling ({color})')
 
-    plt.plot(terms_to_print, execution_times, marker='o')
-    plt.title('Fast Doubling Fibonacci Execution Time')
+if __name__ == "__main__":
+    n = 30
+
+    calculate_execution_times_recursive(n, 'blue')
+    calculate_execution_times_dynamic(n, 'orange')
+    calculate_execution_times_iterative(n, 'green')
+    calculate_execution_times_binet(n, 'red')
+    calculate_execution_times_matrix_power(n, 'purple')
+    calculate_execution_times_fast_doubling(n, 'brown')
+
+    plt.title('Execution Time for Fibonacci Methods')
     plt.xlabel('Term')
     plt.ylabel('Execution Time (milliseconds)')
+    plt.legend()
     plt.show()
-
-
-# Method #1
-# n = 40
-# calculate_execution_times(n)
-
-# Method #2
-# n = 2000
-# calculate_execution_times_dynamic(n)
-
-# Method #3
-# n = 2000
-# calculate_execution_times_iterative(n)
-
-# Method #4
-# n = 10000
-# calculate_execution_times_binet(n)
-
-# Method #5
-# n = 10000
-# calculate_execution_times_matrix_power(n)
-
-# Method #6
-# n = 10000
-# calculate_execution_times_fast_doubling(n)
-
